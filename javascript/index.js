14ab57bb66f6a739cb74917e0508aea5f405e766
Benoit Benezech <benoit.benezech@gmail.com>
2016-07-13 14:20:30 +0200

    base bootstrap stuff

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var bubbles = document.getElementById('bubbles')
+  if (bubbles) {
+    var randomN = function(start, end){
+      return Math.random()*end+start
+    }
+    var i = 0
+    var generateBubble = function(){
+      if(i < 20){
+        var el = document.createElement('div'),
+            size = randomN(5, 10)
+        el.setAttribute('style','width: '+size+'px; height: '+size+'px; left:'+randomN(1, bubbles.offsetWidth-(size+4) )+'px;')
+        el.classList.add('bubble')
+        bubbles.appendChild(el)
+        i++;
+      } else {
+        clearInterval(inter)
+      }
+    }
+
+    console.log('bubbles', bubbles)
+    generateBubble()
+
+    var inter = setInterval(generateBubble, 500);
+  }
+})
